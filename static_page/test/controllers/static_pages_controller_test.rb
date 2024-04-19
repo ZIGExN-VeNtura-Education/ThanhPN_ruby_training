a4e14db8456bc881463b8f0f8d203262685af08a
RioLeon <phungnthanhdev@gmail.com>
2024-04-19 15:09:55 +0700

    Add a full title helper. Maybe

--- static_page/test/controllers/static_pages_controller_test.rb
+++ static_page/test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
@@ -9,20 +8,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
-
   end
 
   test "should get about" do
