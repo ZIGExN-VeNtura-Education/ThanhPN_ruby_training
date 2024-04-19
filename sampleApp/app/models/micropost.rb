adc48ea8ba5bf879a45ffc1bd9939b2655a8a2dc
RioLeon <138135757+RioLeon@users.noreply.github.com>
2024-04-19 15:14:38 +0700

    Rails flavored ruby (#5)
    * Add a full title helper. Maybe
    
    * remove book
    
    * remove book

--- sampleApp/app/models/micropost.rb
+++ sampleApp/app/models/micropost.rb
@@ -2,3 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: {maximum: 140}, presence: true
 end
+
+# model luon so it (not s)
+# controller luon so nhieu (s)
--- static_page/config/routes.rb
+++ static_page/config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#home'
+  root "static_pages#home"
+
+  # namespace scope in routes
+  # So sanh scope / namespace trong route
+  # So sanh res (s) va res (non s)
+  # Co S them index
+  # dung res them method
+  # gioi han khong them method bang them lenh
+  # %i = symbol
+  # dung render trong controller de render view khac
+  # Chuoi noi suy = "#{}"
+  # Xoa tat ca khoang trang cuoi cung strip
+  # Method chuoi
+  # Turbo link chi reload thang nao can reload thang nao. tuy nhien option reload se reload toan bo trang
+  # media type stylesheet tag
+  # Tap trung core: Xu ly chuoi va hash, gem device
+  # No focus test
 end
