edfbfd1d90a6b3b295ca1c3a8e10def7b7e5ab8d
RioLeon <138135757+RioLeon@users.noreply.github.com>
2024-04-24 15:48:47 +0700

    Finish sign-up chapter (#7)

--- static_page/config/routes.rb
+++ static_page/config/routes.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
+  get "users/new"
+  root "static_pages#home"
 
-  get '/help', to: 'static_pages#help', as: 'shit'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  get "/help", to: "static_pages#help", as: "shit"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
-  # namespace scope in routes
-  # So sanh scope / namespace trong route
-  # So sanh res (s) va res (non s)
-  # Co S them index
-  # dung res them method
-  # gioi han khong them method bang them lenh
+  # namespace scope in routes x
+  # So sanh scope / namespace trong route x
+  # So sanh res (s) va res (non s) x
+  # Co S them index x
+  # dung res them method x
+  # gioi han khong them method bang them lenh except/only x
   # %i = symbol
-  # dung render trong controller de render view khac
-  # Chuoi noi suy = "#{}"
-  # Xoa tat ca khoang trang cuoi cung strip
+  # dung render trong controller de render view khac x
+  # Chuoi noi suy = "#{}" x
+  # Xoa tat ca khoang trang cuoi cung strip x
   # Method chuoi
-  # Turbo link chi reload thang nao can reload thang nao. tuy nhien option reload se reload toan bo trang
+  # Turbo link chi reload thang nao can reload thang nao. tuy nhien option reload se reload toan bo trang x
   # media type stylesheet tag
   # Tap trung core: Xu ly chuoi va hash, gem device
   # No focus test
