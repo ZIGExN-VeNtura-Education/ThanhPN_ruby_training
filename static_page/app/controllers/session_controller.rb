907fa6b1b35cce606be778b46cd735b6349c330a
RioLeon <phungnthanhdev@gmail.com>
2024-04-25 17:38:42 +0700

    Finish basic-login chapter

---
+++ static_page/app/controllers/session_controller.rb
@@ -0,0 +1,19 @@
+class SessionController < ApplicationController
+  include SessionHelper
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = "Invalid email or password"
+      render "new"
+    end
+  end
+
+  def destroy
+  end
+end
--- static_page/config/routes.rb
+++ static_page/config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "users/new"
   root "static_pages#home"
 
   get "/help", to: "static_pages#help", as: "shit"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
+  get "/login", to: "session#new"
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -24,6 +26,6 @@ Rails.application.routes.draw do
   # Method chuoi
   # Turbo link chi reload thang nao can reload thang nao. tuy nhien option reload se reload toan bo trang x
   # media type stylesheet tag
-  # Tap trung core: Xu ly chuoi va hash, gem device
+  # Tap trung core: Xu ly chuoi va hash, gem devise
   # No focus test
 end
