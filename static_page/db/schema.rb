59f5c8d1569c77528feeecdb95afdaca0209d7a6
RioLeon <138135757+RioLeon@users.noreply.github.com>
2024-04-29 14:13:11 +0700

    Basic login and advanced login (#8)
    * Finish sign-up chapter
    
    * Finish basic-login chapter
    
    * Finish basic login and advanced login chapter

--- static_page/db/schema.rb
+++ static_page/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_23_082658) do
+ActiveRecord::Schema.define(version: 2024_04_26_062845) do
 
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2024_04_23_082658) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
